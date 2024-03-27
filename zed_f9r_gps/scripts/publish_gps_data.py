c43e6235d4301b699a6e6c373764325480cd009a
Matthew Oujiri <mkoujiri@mtu.edu>
2024-03-27 12:14:20 -0400

    Stripped down and switched to ros-noetic script installation over module installation

--- zed_f9r_gps/scripts/publish_gps_data.py
+++ zed_f9r_gps/scripts/publish_gps_data.py
@@ -1,22 +1,24 @@
-import rclpy
+import rospy
 import serial
-from rclpy.node import Node
 from ublox_gps import UbloxGps
 from std_msgs.msg import String
 from sensor_msgs.msg import NavSatFix
 
-class GPSPublisher(Node):
+class GPSPublisher():
 
     def __init__(self):
-        super().__init__('gps_publisher')
-        self.publisher_ = self.create_publisher(NavSatFix, 'gps/fix', 10)
-        self.port = serial.Serial('/dev/serial0', baudrate=38400, timeout=1)
+        self.publisher_ = rospy.Publisher('/gps/fix', NavSatFix, queue_size=10)
+        rospy.init_node('gps_publisher')
+        # windows serial
+        #   self.port = serial.Serial('/dev/serial0', baudrate=38400, timeout=1)
+        # linux serial
+        self.port = serial.Serial('/dev/ttyACM0', baudrate=38400, timeout=1)
         self.gps = UbloxGps(self.port)
 
     def publish(self):
         try: 
             print("Listening for UBX Messages")
-            while True:
+            while not rospy.is_shutdown():
                 try:
                     geo = self.gps.geo_coords()
                     cov = self.gps.geo_cov()
@@ -34,21 +36,18 @@ class GPSPublisher(Node):
                     msg.position_covariance = [EE, NE, ED, NE, NN, ND, ED, ND, DD]
                     msg.position_covariance_type = 3
                     self.publisher_.publish(msg)
+                    # rate.sleep()
                 except (ValueError, IOError) as err:
                     print(err)
         finally:
-            port.close()
+            self.port.close()
 
 
 def main(args=None):
-    rclpy.init(args=args)
+    # rclpy.init(args=args)
 
     gps_publisher = GPSPublisher()
     gps_publisher.publish()
-
-    gps_publisher.destroy_node()
-    rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
